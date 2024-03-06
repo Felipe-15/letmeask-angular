c8db2ce40a177510846199192aff9a55704d2302
Felipe Souza <felipesouza036@gmail.com>
2024-03-06 11:20:21 -0300

    build: Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#835AFD",
+          400: "#6F4BD8",
+        },
+        danger: {
+          500: "#E73F5D",
+          400: "#D73754",
+        },
+        gray: {
+          600: "#737380",
+          500: "#A8A8B3",
+          400: "#DBDCDD",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
