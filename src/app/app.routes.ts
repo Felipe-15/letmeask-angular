b865a78b1f7e222451e468d316e7ff9ed80bf589
Felipe Souza <felipesouza036@gmail.com>
2024-03-06 12:12:44 -0300

    feat: Add home layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { EnterRoomComponent } from './components/enter-room/enter-room.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+import { LayoutComponent } from './templates/home/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home/select-room',
+  },
+  {
+    path: 'home',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'select-room',
+        component: EnterRoomComponent,
+      },
+      {
+        path: 'create-room',
+        component: CreateRoomComponent,
+      },
+    ],
+  },
+];
