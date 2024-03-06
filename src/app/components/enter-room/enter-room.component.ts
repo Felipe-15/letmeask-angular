35e4ecc9d398084dc49e644d02320866cb42e266
Felipe Souza <felipesouza036@gmail.com>
2024-03-06 14:26:29 -0300

    feat: Add enter room structure

--- src/app/components/enter-room/enter-room.component.ts
+++ src/app/components/enter-room/enter-room.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { IconsModule } from '../../icons/icons.module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-enter-room',
   standalone: true,
-  imports: [],
+  imports: [IconsModule, RouterLink],
   templateUrl: './enter-room.component.html',
-  styleUrl: './enter-room.component.css'
+  styleUrl: './enter-room.component.css',
 })
-export class EnterRoomComponent {
-
-}
+export class EnterRoomComponent {}
