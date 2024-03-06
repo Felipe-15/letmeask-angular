0557cc418814382d4127c930e46c26568ff5ab36
Felipe Souza <felipesouza036@gmail.com>
2024-03-06 14:31:17 -0300

    feat: Add create room structure

--- src/app/components/create-room/create-room.component.ts
+++ src/app/components/create-room/create-room.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-room',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './create-room.component.html',
-  styleUrl: './create-room.component.css'
+  styleUrl: './create-room.component.css',
 })
-export class CreateRoomComponent {
-
-}
+export class CreateRoomComponent {}
