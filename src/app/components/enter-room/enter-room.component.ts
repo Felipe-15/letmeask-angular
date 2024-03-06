e912465befed9fe2a4bb5b0646abd4d1f421cd08
Felipe Souza <felipesouza036@gmail.com>
2024-03-06 12:13:08 -0300

    feat: Add Enter Room component

---
+++ src/app/components/enter-room/enter-room.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-room',
+  standalone: true,
+  imports: [],
+  templateUrl: './enter-room.component.html',
+  styleUrl: './enter-room.component.css'
+})
+export class EnterRoomComponent {
+
+}
